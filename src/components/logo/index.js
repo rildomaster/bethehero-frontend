7ca9692e720c33ee852ab1c647ae2cddbf42951a
Rildo Gomes <rildo_master_pe@hotmail.com>
2020-03-28 21:11:53 -0300

    Commit Inicial

---
+++ src/components/logo/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function Logo() {
+    return (
+        <img src={logoImg} alt="Be The Hero"/>
+    );
+};
